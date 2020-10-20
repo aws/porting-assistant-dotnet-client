2f38e555f88cc1c77beb5cc84983325b371020cc
Andrew Wan <lwwnz@amazon.com>
2020-10-19 23:07:37 -0700

    Removing the static varibale for api index

---
+++ src/PortingAssistant.Client.Common/Model/PackageDetailsWithApiIndexs.cs
@@ -0,0 +1,13 @@
+﻿using PortingAssistant.Client.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortingAssistant.Client.Model
+{
+    public class PackageDetailsWithApiIndexs
+    {
+        public PackageDetails PackageDetails { get; set; }
+        public Dictionary<string, int> IndexDict { get; set; }
+    }
+}
