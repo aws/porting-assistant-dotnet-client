cc7820539e0f1aa395d79b3b3e1344382c1dd9b9
Andrew Wan <lwwnz@amazon.com>
2020-10-21 17:18:09 -0700

    Remove the duplicate IsBuildFailed variable

--- src/PortingAssistant.Client.Common/Model/ProjectAnalysisResult.cs
+++ src/PortingAssistant.Client.Common/Model/ProjectAnalysisResult.cs
@@ -6,7 +6,6 @@ namespace PortingAssistant.Client.Model
     public class ProjectAnalysisResult : ProjectDetails
     {
         public List<string> Errors { get; set; }
-        public bool IsBuildFailed { get; set; }
         public List<SourceFileAnalysisResult> SourceFileAnalysisResults { get; set; }
         public Dictionary<PackageVersionPair, Task<PackageAnalysisResult>> PackageAnalysisResults { get; set; }
     }
