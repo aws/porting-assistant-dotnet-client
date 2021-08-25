11d28a1d54c0af503ee3714e666b0de3d1582287
Shanshan He <shanshanhebit@gmail.com>
2021-08-24 17:42:28 -0700

    Add ApplicationGuid to `solutionDetails`.

--- src/PortingAssistant.Client.Common/Model/SolutionDetails.cs
+++ src/PortingAssistant.Client.Common/Model/SolutionDetails.cs
@@ -7,6 +7,12 @@ namespace PortingAssistant.Client.Model
         public string SolutionName { get; set; }
         public string SolutionFilePath { get; set; }
         public string SolutionGuid { get; set; }
+        /*
+         * Used to uniquely identify the solution PA is analyzing.
+         * Its value is the same as SolutionGuid if SolutionGuid presents.
+         * Otherwise, we compute the hash based on all the ProjectGuids.
+         */
+        public string ApplicationGuid { get; set; }
         public string RepositoryUrl { get; set; }
         public List<string> FailedProjects { get; set; }
         public List<ProjectDetails> Projects { get; set; }
