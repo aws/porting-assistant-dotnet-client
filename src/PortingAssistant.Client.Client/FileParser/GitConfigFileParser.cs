ffc70142b1e997168bf014cb5d8639f09868e0ed
Mark Fawaz <markfawa@amazon.com>
2021-12-03 09:39:17 -0800

    Check that a remote repo exists before attempting to read it in the git configuration

--- src/PortingAssistant.Client.Client/FileParser/GitConfigFileParser.cs
+++ src/PortingAssistant.Client.Client/FileParser/GitConfigFileParser.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using LibGit2Sharp;
 
 namespace PortingAssistant.Client.Client.FileParser
@@ -32,7 +34,9 @@ namespace PortingAssistant.Client.Client.FileParser
             }
 
             var repo = new Repository(gitRepositoryRootPath);
-            return repo.Config.Get<string>(new[] { "remote", "origin", "url" }).Value;
+
+            var configEntries = new List<ConfigurationEntry<string>>(repo.Config.Find("remote.origin.url"));
+            return configEntries.FirstOrDefault()?.Value;
         }
     }
 }
