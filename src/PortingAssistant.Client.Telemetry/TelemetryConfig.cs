262e0902bb06b253302b09fb456f89e9b8c794f3
Chris <longachr@amazon.com>
2022-01-13 15:26:54 -0800

    fix: use session token in metrics request (#1006)
    * fix: use session token in metrics request
    * fix: use aws sdk signer

---
+++ src/PortingAssistant.Client.Telemetry/TelemetryConfig.cs
@@ -0,0 +1,19 @@
+﻿
+using Amazon.Runtime;
+using Amazon.Util.Internal;
+
+namespace PortingAssistant.Client.Telemetry
+{
+    public class TelemetryConfig : ClientConfig
+    {
+        private static readonly string userAgentString =
+            InternalSDKUtils.BuildUserAgentString("3.5.0.9");
+        public TelemetryConfig()
+        {
+            AuthenticationServiceName = "execute-api";
+        }
+        public override string RegionEndpointServiceName => "encore";
+        public override string ServiceVersion => "";
+        public override string UserAgent => userAgentString;
+    }
+}
---
+++ src/PortingAssistant.Client.Telemetry/TelemetryRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon.Runtime;
+
+namespace PortingAssistant.Client.Telemetry
+{
+    public class TelemetryRequest : AmazonWebServiceRequest
+    {
+        public string Content;
+        public readonly RequestMetadata RequestMetadata;
+
+        public TelemetryRequest(string serviceName, string content)
+        {
+            Content = content;
+            RequestMetadata = new RequestMetadata(serviceName);
+        }
+    }
+
+    public class RequestMetadata
+    {
+        public string Service;
+        public readonly string Version = "1.0";
+        public readonly string Token = "12345678";
+        public readonly string Created = DateTime.UtcNow.Date.ToString("yyyy-MM-dd");
+        public RequestMetadata(string serviceName)
+        {
+            Service = serviceName;
+        }
+    }
+}
