12106415dbb2f21873a5a3f8cd6b70abedc584b0
Jon Louie <louiejon@amazon.com>
2022-01-19 12:56:29 -0800

    add nuget version helper classes and tests

---
+++ src/PortingAssistant.Client.Analysis/Utils/NugetVersionExtensions.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NuGet.Versioning;
+
+namespace PortingAssistant.Client.Analysis.Utils
+{
+    public static class NugetVersionExtensions
+    {
+        private static readonly HashSet<string> ZeroVersions = new HashSet<string>
+        {
+            "0.0.0",
+            "0.0.0.0"
+        };
+
+        public static bool IsZeroVersion(this NuGetVersion thisVersion)
+        {
+            return ZeroVersions.Contains(thisVersion.ToString());
+        }
+
+        public static bool IsGreaterThanOrEqualTo(this NuGetVersion thisVersion, string otherVersion)
+        {
+            if (NuGetVersion.TryParse(otherVersion, out var validOtherVersion))
+            {
+
+                return validOtherVersion.IsZeroVersion() 
+                       || thisVersion.IsGreaterThanOrEqualTo(validOtherVersion);
+            }
+
+            return false;
+        }
+
+        public static bool IsGreaterThanOrEqualTo(this NuGetVersion thisVersion, NuGetVersion otherVersion)
+        {
+            return thisVersion.CompareTo(otherVersion) >= 0;
+        }
+
+        public static bool IsGreaterThan(this NuGetVersion thisVersion, string otherVersion)
+        {
+            if (otherVersion == "0.0.0" || otherVersion == "0.0.0.0")
+            {
+                return true;
+            }
+
+            if (NuGetVersion.TryParse(otherVersion, out var validOtherVersion))
+            {
+                return thisVersion.IsGreaterThan(validOtherVersion);
+            }
+
+            return false;
+        }
+
+        public static bool IsGreaterThan(this NuGetVersion thisVersion, NuGetVersion otherVersion)
+        {
+            return thisVersion.CompareTo(otherVersion) > 0;
+        }
+
+        public static bool IsLessThanOrEqualTo(this NuGetVersion thisVersion, string otherVersion)
+        {
+            if (thisVersion.ToString() == "0.0.0" || thisVersion.ToString() == "0.0.0.0")
+            {
+                return true;
+            }
+
+            if (NuGetVersion.TryParse(otherVersion, out var validOtherVersion))
+            {
+                return thisVersion.IsLessThanOrEqualTo(validOtherVersion);
+            }
+
+            return false;
+        }
+
+        public static bool IsLessThanOrEqualTo(this NuGetVersion thisVersion, NuGetVersion otherVersion)
+        {
+            return thisVersion.CompareTo(otherVersion) <= 0;
+        }
+
+        public static bool HasSameMajorAs(this NuGetVersion thisVersion, string otherVersion)
+        {
+            if (NuGetVersion.TryParse(otherVersion, out var validOtherVersion))
+            {
+                return thisVersion.HasSameMajorAs(validOtherVersion);
+            }
+
+            return false;
+        }
+
+        public static bool HasSameMajorAs(this NuGetVersion thisVersion, NuGetVersion otherVersion)
+        {
+            return thisVersion.Major.CompareTo(otherVersion.Major) == 0;
+        }
+
+        public static IEnumerable<string> FindLowerOrEqualCompatibleVersions(this NuGetVersion thisVersion, IEnumerable<string> compatibleNugetVersions)
+        {
+            return compatibleNugetVersions.Where(v =>
+            {
+                if (NuGetVersion.TryParse(v, out var validNugetVersion))
+                {
+                    return validNugetVersion.IsLessThanOrEqualTo(thisVersion);
+                }
+
+                return false;
+            });
+        }
+
+        public static bool HasLowerOrEqualCompatibleVersion(this NuGetVersion thisVersion, IEnumerable<string> compatibleNugetVersions)
+        {
+            return thisVersion.FindLowerOrEqualCompatibleVersions(compatibleNugetVersions).Any();
+        }
+
+        public static IEnumerable<string> FindGreaterCompatibleVersions(this NuGetVersion thisVersion, IEnumerable<string> compatibleNugetVersions)
+        {
+            return compatibleNugetVersions.Where(v =>
+            {
+                if (NuGetVersion.TryParse(v, out var validNugetVersion))
+                {
+                    return validNugetVersion.IsGreaterThan(thisVersion);
+                }
+
+                return false;
+            });
+        }
+    }
+}
---
+++ src/PortingAssistant.Client.Analysis/Utils/NugetVersionHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NuGet.Versioning;
+
+namespace PortingAssistant.Client.Analysis.Utils
+{
+    public class NugetVersionHelper
+    {
+        public static NuGetVersion GetMaxVersion(IEnumerable<string> nugetVersions)
+        {
+            var parsedVersions = nugetVersions.Select(v =>
+            {
+                if (NuGetVersion.TryParse(v, out var validVersion))
+                {
+                    return validVersion;
+                }
+
+                return null;
+            }).Where(v => v!= null);
+
+            // Returns null if there are no valid nugetVersions
+            return GetMaxVersion(parsedVersions);
+        }
+
+        public static NuGetVersion GetMaxVersion(IEnumerable<NuGetVersion> nugetVersions)
+        {
+            return nugetVersions.Max();
+        }
+        
+        public static bool HasLowerCompatibleVersionWithSameMajor(NuGetVersion nugetVersion, IEnumerable<string> compatibleNugetVersions)
+        {
+            return compatibleNugetVersions.Any(v =>
+                nugetVersion.IsGreaterThanOrEqualTo(v)
+                && nugetVersion.HasSameMajorAs(v)
+            );
+        }
+    }
+}
