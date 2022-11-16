6a758246e4734f8cf6de068a0ba3f39e1e43a299
longachr <longachr@amazon.com>
2022-11-16 09:07:12 -0800

    update gitconfig file tests
    cr: https://code.amazon.com/reviews/CR-79753526

--- tests/PortingAssistant.Client.UnitTests/GitConfigFileParserTest.cs
+++ tests/PortingAssistant.Client.UnitTests/GitConfigFileParserTest.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using NUnit.Framework;
 using PortingAssistant.Client.Client.FileParser;
 
@@ -12,7 +13,7 @@ namespace PortingAssistant.Client.UnitTests
             string gitRootPath = GitConfigFileParser.getGitRepositoryRootPath(
                 Directory.GetCurrentDirectory());
             string expectedRootPathEnding = Path.Combine("porting-assistant-dotnet-client", ".git");
-            Assert.IsTrue(gitRootPath.Contains(expectedRootPathEnding));
+            Assert.IsTrue(gitRootPath.Contains(expectedRootPathEnding, StringComparison.OrdinalIgnoreCase));
         }
 
         [Test]
@@ -20,16 +21,6 @@ namespace PortingAssistant.Client.UnitTests
         {
             string gitRootPath = GitConfigFileParser.getGitRepositoryRootPath(@"C:\\RandomFile\\Path\\solution.sln");
             Assert.AreEqual(null, gitRootPath);
-        }
-
-        [Test]
-        public void getGitRepositoryUrl_Returns_Expected_Url()
-        {
-            string gitRootPath = GitConfigFileParser.getGitRepositoryRootPath(
-                Directory.GetCurrentDirectory());
-            string gitUrl = GitConfigFileParser.getGitRepositoryUrl(gitRootPath);
-            Assert.IsTrue(gitUrl.Contains("https://github.com/aws/porting-assistant-dotnet-client") || 
-                gitUrl.Contains("git@github.com:aws/porting-assistant-dotnet-client.git"));
         }
 
         [Test]
