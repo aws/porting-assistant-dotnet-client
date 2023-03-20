fd98c58d92b83e22a576135162cdf70c38e03396
Jiayu Wang <wwangjy@amazon.com>
2023-03-20 12:25:29 -0700

    change back to prod bucket

--- src/PortingAssistant.Client.Common/Model/PortingAssistantConfiguration.cs
+++ src/PortingAssistant.Client.Common/Model/PortingAssistantConfiguration.cs
@@ -12,8 +12,8 @@ namespace PortingAssistant.Client.Model
             this.UseInternalNuGetServer = false;
             this.DataStoreSettings = new DataStoreSettings
             {
-                HttpsEndpoint = "https://s3.us-west-2.amazonaws.com/portingassistantdatastorebeta/",
-                S3Endpoint = "portingassistantdatastorebeta",
+                HttpsEndpoint = "https://s3.us-west-2.amazonaws.com/aws.portingassistant.dotnet.datastore/",
+                S3Endpoint = "aws.portingassistant.dotnet.datastore",
                 GitHubEndpoint = "https://raw.githubusercontent.com/aws/porting-assistant-dotnet-datastore/master/"
             };
             this.InternalNuGetServerSettings = new NuGetServerSettings
